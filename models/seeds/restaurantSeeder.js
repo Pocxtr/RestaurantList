995664f57b59b0c2a4970feb0bd31488a301743b
Wei <pocxtr@gmail.com>
2020-11-06 16:58:27 +0800

    Add seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,42 @@
+const User = require('../user')
+const Restaurant = require('../restaurant')
+const bcrypt = require('bcryptjs')
+const restaurantList = require('./restaurant.json')
+const db = require('../../config/mongoose')
+const SEED_USERS = [
+  {
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    email: 'user2@example.com',
+    password: '12345678'
+  }
+]
+
+db.once('open', () => {
+  const restaurants = restaurantList.results
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USERS[0].password, salt))
+    .then(hash => {
+      SEED_USERS.forEach(SEED_USER => SEED_USER.password = hash)
+      return User.insertMany(SEED_USERS)
+    })
+    .then(users => {
+      return Promise.all(restaurants.map((restaurant, index) => {
+        if (index < 3) {
+          restaurant.userId = users[0]._id
+          return Restaurant.create(Object.assign({}, restaurant))
+        }
+        if (index < 6) {
+          restaurant.userId = users[1]._id
+          return Restaurant.create(Object.assign({}, restaurant))
+        }
+      }))
+    })
+    .then(() => {
+      console.log('Seeder done.')
+      process.exit()
+    })
+})
