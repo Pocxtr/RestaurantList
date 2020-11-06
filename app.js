b9a77ba1d557a6dab8358b1f256832264d9cc842
Wei <pocxtr@gmail.com>
2020-11-07 02:20:22 +0800

    Add user registration feature

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const exhbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
@@ -10,8 +12,9 @@ const port = 3000
 app.engine('hbs', exhbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
-// setting static files
+// setting middleware
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(routes)
 
