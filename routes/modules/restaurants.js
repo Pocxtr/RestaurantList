3dfcc2988a0d17af66d67b277332486cd072620e
Wei <pocxtr@gmail.com>
2020-11-07 15:01:53 +0800

    Add read detail function

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -3,6 +3,17 @@ const router = express.Router()
 
 const Restaurant = require('../../models/restaurant')
 
+//Route to read detail
+router.get('/:id', (req, res) => {
+    const userId = req.user._id
+    const _id = req.params.id
+    Restaurant.findOne({ _id, userId })
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
+    .catch(err => console.log(err))
+})
+
+//Route to create restaurant
 router.get('/new', (req, res) => {
     res.render('new')
 })
