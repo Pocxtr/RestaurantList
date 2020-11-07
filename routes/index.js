3e3ab970b1ab0fae30c4dcf6ef236d863413400e
Wei <pocxtr@gmail.com>
2020-11-07 21:22:55 +0800

    Add search restaurant function

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const users = require('./modules/users')
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const search = require('./modules/search')
 const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
 router.use('/restaurants', authenticator, restaurants)
+router.use('/search', authenticator, search)
 router.use('/auth', auth)
 router.use('/', authenticator, home)
 
---
+++ routes/modules/search.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+const Restaurant = require('../../models/restaurant')
+
+// Set route to search restaurant
+router.get('/', (req, res) => {
+    const userId = req.user._id
+    const keyword = req.query.keyword
+    Restaurant.find({ userId })
+    .lean()
+    .then(restaurants => {
+        const target = restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()))
+        res.render('index', { restaurants: target, keyword })
+    })
+    .catch(err => console.error(err))
+})
+
+module.exports = router
