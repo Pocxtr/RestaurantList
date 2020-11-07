2704dcfa1ce99611ff7e8331366c032a65609362
Wei <pocxtr@gmail.com>
2020-11-07 13:43:05 +0800

    Add bcrypt

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.success_msg = req.flash('success_msg')
   res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.error = req.flash('error')
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 const User = require('../../models/user')
 
@@ -9,7 +10,8 @@ router.get('/login', (req, res) => {
 })
 router.post('/login', passport.authenticate('local', {
     successRedirect: '/',
-    failureRedirect: '/users/login'
+    failureRedirect: '/users/login',
+    failureFlash: true
 }))
 router.get('/register', (req, res) => {
     res.render('register')
@@ -42,11 +44,14 @@ router.post('/register', (req, res) => {
                 confirmPassword
             })
         } else {
-            return User.create({
-                name,
-                email,
-                password
-            })
+            return bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(password, salt))
+            .then(hash => User.create({
+              name,
+              email,
+              password: hash
+            }))
             .then(()=> res.redirect('/'))
             .catch(err => console.log(err))
         }
