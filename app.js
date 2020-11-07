e4d9db0cd18b210e3ccdb35ed350e68e3f7c5466
Wei <pocxtr@gmail.com>
2020-11-07 10:26:37 +0800

    Add express-seesion & dotenv

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const exhbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
-const port = 3000
 
 // setting template engine
 app.engine('hbs', exhbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+
+// setting session
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  }))
 
 // setting middleware
 app.use(express.static('public'))
@@ -18,6 +29,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(routes)
 
-app.listen(port, () => {
-    console.log(`App is running on http://localhost:${port}`)
+app.listen(process.env.port, () => {
+    console.log(`App is running on http://localhost:${process.env.port}`)
 })
